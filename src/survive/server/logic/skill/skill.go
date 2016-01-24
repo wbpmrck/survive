fb5e804aa8dff88013aecb146002b8329a5d2594
kaicui <wbpmrck2003@gmail.com>
2016-01-24 15:46:31 +0800

    完成ChooseByWeak选择器。

--- src/survive/server/logic/skill/skill.go
+++ src/survive/server/logic/skill/skill.go
@@ -1,28 +1,84 @@
 
-//技能是由若干个技能项构成
-//每个技能项，是一个技能作用对象chooser，和一堆效果的集合（一个技能项，代表向一堆对象释放的效果）
-//技能可以升级，要保存当前的技能等级、经验，总级数，每一级需要经验等信息
 package skill
 import (
-	"survive/server/logic/character"
-"survive/server/logic/skill/targetChoose"
+	"survive/server/logic/skill/effect"
+	"survive/server/logic/skill/targetChoose"
+	"fmt"
 )
+/*
+	关于技能的设计：
+	1、技能其实是效果、和作用对象的封装
+	2、技能负责产生效果，但它无法撤消效果。效果是真正实现技能的核心，它自己决定自己何时被消灭，当然，也可以被主动消灭
+	3、技能还有一个主要的目的，是管理效果的参数，比如技能可以有等级和熟练度的概念，借此来影响内部效果的配置值
+ */
 
-//代表一个技能项
-type SkillItem interface {
-	SetTargetChooser(chooser targetChoose.TargetChooser) //设定技能的选择目标的策略
-	Install(from *character.Character,targets []*character.Character) //install的过程，就是给每一个target创建effect对象，并PutOn的过程
-	GetInfo() string //获取描述信息
-	Update(args ...interface{}) //进行技能的生效处理(Update可能多次被调用)
-	UnInstall() //技能结束处理，表示该技能释放完毕
+//代表一个技能
+//一个技能，是由若干个效果项构成的
+type Skill struct {
+	Carrier SkillCarrier //这个技能所属的对象(是对象的技能)
+	Level int //当前技能等级
+	Exp int //当前技能熟练度
+	Items []*SkillItem
+	Name string
+}
+//技能安装
+func(self *Skill) Install(from SkillCarrier){
+	self.Carrier = from
+	//初始化每个效果
+	if self.Items != nil && len(self.Items)>0{
+		for _,item := range self.Items{
+			item.Install(from)
+		}
+	}
+}
+//获取技能信息(暂时就返回名字+技能列表)
+func(self *Skill) GetInfo() string{
+	return fmt.Sprintf("[%v]%v",self.Name,self.Items)
+}
+//获取描述信息
+func(self *Skill) AddSkillItem(item *SkillItem) {
+	if item != nil{
+		if self.Items != nil{
+			self.Items = make([]*SkillItem,0)
+		}
+		self.Items = append(self.Items,item)
+	}
 }
 
-//代表一个技能。
-//一个技能可以含有若干个技能项
-//技能的释放，就是对所有技能项目的激活
-type Skill interface {
-	Install(from *character.Character,targets []*character.Character)
-	GetInfo() string //获取描述信息
-	Update(args ...interface{}) //进行技能的生效处理(Update可能多次被调用)
-	UnInstall() //技能结束处理，表示该技能释放完毕
+
+//表示一个技能项
+type SkillItem struct {
+	SkillParent *Skill //指向包含自己的技能项
+	PluginStep Step //技能项插入技能持有者动作的时机(skillItem通过注册skillCarrier的这个事件,来决定何时调用effect)
+	Chooser targetChoose.TargetChooser //在固定时机的基础上，chooser决定效果的作用对象
+	EffectItems []effect.Effect
+}
+//获取描述信息
+func(self *SkillItem) String() string{
+	return fmt.Sprintf("作用时机:%v \r\n作用对象:%v \r\n效果项:%v",self.PluginStep.StepDesc,self.Chooser.String(),self.EffectItems)
+}
+func(self *SkillItem) Install(){
+	//在技能持有者身上订阅触发技能的事件
+	if self.SkillParent.Carrier != nil{
+		//在指定的阶段
+		stepName:=self.PluginStep.StepName
+		self.SkillParent.Carrier.On(stepName,func(contextParams ...interface{}) (isCancel bool,handleResult string){
+			//找到指定的作用对象
+			targets,err := self.Chooser.Choose(self.SkillParent.Carrier,stepName,contextParams...)
+			if err == nil && targets!= nil && len(targets)>0{
+				for _,target := range targets{
+					//对每一个效果，使用PutOn进行触发
+					for _,effectItem := range self.EffectItems{
+						effectItem.PutOn(self.SkillParent.Carrier,target)
+					}
+				}
+			}
+		})
+	}
+}
+//定义技能项的释放时机
+//后续应该初始化若干个释放时机以供选择
+type Step struct {
+	StepName string //插入时机名称
+	StepDesc string //插入时机描述
 }
--- src/survive/server/logic/skill/skillCarrier.go
+++ src/survive/server/logic/skill/skillCarrier.go
@@ -1,8 +1,24 @@
 package skill
+import "survive/server/logic/rule/event"
 
 /*
-	定义一种接口，以及其标准实现。
-	该接口描述了一切可以释放技能的实体应该具备的能力
+	定义一种技能持有者的标准实现。
+	该接口描述了一切可以释放技能的实体应该具备的能力。
+	其他对象可以直接内嵌本结构，来具备使用技能的能力
  */
+
+type SkillCarrier interface {
+	event.EventEmitter
+	//获取所有技能
+	GetAllSkills() []*Skill
+}
+
+type SkillCarrierBase struct {
+	*event.EventEmitterBase
+	skills []*Skill
+}
+func(self *SkillCarrierBase) GetAllSkills() []*Skill{
+	return self.skills
+}
 
 
